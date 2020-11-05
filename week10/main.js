079babce4e58108d52805bde8d908d8bf59250ff
darlene tjahjo <darlenetjahjo@darlenes-Air.fios-router.home>
2020-11-04 20:01:24 -0500

    pair

---
+++ week10/main.js
@@ -0,0 +1,35 @@
+// global variables
+let selection = 0
+
+function update() {
+	// local variables
+	let options = document.querySelectorAll('.option')
+	console.log(options)
+	options.forEach(item => item.onclick=function() {
+		selection = item.id
+		console.log(selection)
+	})
+}
+
+update();
+
+
+function update () {
+	// local variables
+	let second = document.querySelectorAll('.second')
+	console.log(second)
+	second.forEach(item => item.onmouseover=function() {
+	angry.innerHTML='Very'
+	confused.innerHTML='Extremely'
+	hopeless.innerHTML='Unfortunately'
+
+	})
+
+	second.forEach(item => item.onmouseout=function() {
+	angry.innerHTML='Angry'
+	confused.innerHTML='Confused'
+	hopeless.innerHTML='Hopeless'
+		
+	})
+}
+
